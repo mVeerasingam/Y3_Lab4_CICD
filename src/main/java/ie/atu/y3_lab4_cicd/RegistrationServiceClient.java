776455358bdde18e2157b56d35b98f4087d877c9
Mark Veerasingam <markveer70@gmail.com>
2023-10-11 15:33:13 +0100

    updated code from example lab

---
+++ src/main/java/ie/atu/y3_lab4_cicd/RegistrationServiceClient.java
@@ -0,0 +1,11 @@
+package ie.atu.y3_lab4_cicd;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "Confirmation-service", url = "http://localhost:8082")
+public interface RegistrationServiceClient {
+    @PostMapping("/confirm")
+    String someDetails(@RequestBody UserDetails userDetails);
+}
