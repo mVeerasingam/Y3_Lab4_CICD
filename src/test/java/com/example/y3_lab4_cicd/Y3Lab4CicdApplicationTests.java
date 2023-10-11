c63aadb84fdd64f3f1902dc3c3e34c0c9908a6a5
Mark Veerasingam <markveer70@gmail.com>
2023-10-11 14:49:35 +0100

    First Commit

---
+++ src/test/java/com/example/y3_lab4_cicd/Y3Lab4CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.y3_lab4_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Y3Lab4CicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
