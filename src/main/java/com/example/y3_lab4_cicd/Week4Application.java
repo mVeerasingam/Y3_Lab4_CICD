a0770608f43bdb45be6f3cd276065214b5dc0035
Mark Veerasingam <markveer70@gmail.com>
2023-10-11 14:52:34 +0100

    added psvm and created new class

---
+++ src/main/java/com/example/y3_lab4_cicd/Week4Application.java
@@ -0,0 +1,14 @@
+package com.example.y3_lab4_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class Week4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week4Application.class, args);
+    }
+}
